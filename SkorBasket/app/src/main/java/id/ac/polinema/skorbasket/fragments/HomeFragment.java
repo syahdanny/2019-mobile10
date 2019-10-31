a35074974cb3c0e6eeb1ae145da94ef6da0c44d8
syahdanny <syahdanny11@gmail.com>
2019-10-31 14:06:09 +0700

    skor basket telah selesai

--- SkorBasket/app/src/main/java/id/ac/polinema/skorbasket/fragments/HomeFragment.java
+++ SkorBasket/app/src/main/java/id/ac/polinema/skorbasket/fragments/HomeFragment.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -41,6 +43,7 @@ public class HomeFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 							 Bundle savedInstanceState) {
 		// Inflate the layout for this fragment
+		sharedScore = ViewModelProviders.of(requireActivity()).get(SharedScore.class);
 		return inflater.inflate(R.layout.fragment_home, container, false);
 	}
 
@@ -53,5 +56,33 @@ public class HomeFragment extends Fragment {
 		scoreSatuHome = view.findViewById(R.id.scoreSatuHome);
 
 		// Tambahkan logic tombol di bagian bawah ini
+		sharedScore.getScoreHome().observe(requireActivity(), new Observer<Integer>() {
+			@Override
+			public void onChanged(Integer score) {
+				scoreHome.setText(String.valueOf(score));
+				scoreDefault = score;
+			}
+		});
+
+		scoreDuaHome.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				sharedScore.setScoreHome(scoreDefault + 2);
+			}
+		});
+
+		scoreTigaHome.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				sharedScore.setScoreHome(scoreDefault + 3);
+			}
+		});
+
+		scoreSatuHome.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				sharedScore.setScoreHome(scoreDefault + 1);
+			}
+		});
 	}
 }
